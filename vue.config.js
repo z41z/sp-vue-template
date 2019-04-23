b193b2bd14f700a75a46de428fed65999702641d
z41zP <isecfe@gmail.com>
2019-04-23 16:03:54 +0800

    add index.html

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,15 @@ module.exports = {
         }
       }
     }
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0] = {
+          template: 'index.html'
+        }
+        return args
+      })
   }
 }
